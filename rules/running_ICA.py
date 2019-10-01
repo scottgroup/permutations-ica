1700d2dede40b003143c918cab5988263e99b6be
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2019-10-01 10:48:02 -0400

    Correction issues with cluster jobs

--- rules/running_ICA.py
+++ rules/running_ICA.py
@@ -1,6 +1,6 @@
 from snakemake.io import expand
 
-def get_ICA_running(config, dataset):
+def get_ICA_running(configs, datasets):
     """
     Return all necessary files for computations of the model.
 
@@ -10,29 +10,31 @@ def get_ICA_running(config, dataset):
     """
     files = list()
 
-    # Loading config
-    config = config['ICA_datasets'][dataset]['params']
-    config['dataset'] = dataset
+    for dataset in datasets:
+        
+        # Loading config
+        config = configs['ICA_datasets'][dataset]['params']
+        config['dataset'] = dataset
 
-    # Adding M list to params
-    config['M'] = list(range(config['min'], config['max']+1))
+        # Adding M list to params
+        config['M'] = list(range(config['min'], config['max']+1))
 
-    # Calculating all ICA_run
-    ICA_run = list()
-    ICA_run.extend(expand("M{M}_n{n}_std{std}", **config))
-    ICA_run.extend(expand("combine_{min}to{max}_n{n}_std{std}", **config))
-    config['ICA_run'] = ICA_run
+        # Calculating all ICA_run
+        ICA_run = list()
+        ICA_run.extend(expand("M{M}_n{n}_std{std}", **config))
+        ICA_run.extend(expand("combine_{min}to{max}_n{n}_std{std}", **config))
+        config['ICA_run'] = ICA_run
 
-    # ICA model for a specific M
-    path = "results/ICA/{ICAmethod}/{dataset}/M{M}_n{n}_std{std}/components.tsv"
-    files.extend(expand(path, **config))
+        # ICA model for a specific M
+        path = "results/ICA/{ICAmethod}/{dataset}/M{M}_n{n}_std{std}/components.tsv"
+        files.extend(expand(path, **config))
 
-    # ICA model for combined M
-    path = "results/ICA/{ICAmethod}/{dataset}/combine_{min}to{max}_n{n}_std{std}/components.tsv"
-    files.extend(expand(path, **config))
+        # ICA model for combined M
+        path = "results/ICA/{ICAmethod}/{dataset}/combine_{min}to{max}_n{n}_std{std}/components.tsv"
+        files.extend(expand(path, **config))
 
-    # ICA projections of components
-    path = "results/ICA/{ICAmethod}/{dataset}/{ICA_run}/filtered_components/sigma_{sigma}/projection.tsv"
-    files.extend(expand(path, **config))
+        # ICA projections of components
+        path = "results/ICA/{ICAmethod}/{dataset}/{ICA_run}/filtered_components/sigma_{sigma}/projection.tsv"
+        files.extend(expand(path, **config))
 
     return files
